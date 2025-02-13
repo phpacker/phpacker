18d38f81645ab362c42e1c153a8bcb2ccbb62e4a
gwleuverink <willem@leuver.ink>
2025-02-13 01:26:17 +0100

    format

--- src/Command/Build.php
+++ src/Command/Build.php
@@ -8,9 +8,9 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use function Laravel\Prompts\error;
 use function Laravel\Prompts\select;
 use function Laravel\Prompts\multiselect;
-use function Laravel\Prompts\error;
 
 #[AsCommand(
     name: 'build',
@@ -28,32 +28,25 @@ class Build extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument(
-                'platform',
-                InputArgument::OPTIONAL,
-                'Target platform',
-            )
-            ->addArgument(
-                'architectures',
-                InputArgument::IS_ARRAY | InputArgument::OPTIONAL,
-                'Target architectures',
-            );
+            ->addArgument('platform', InputArgument::OPTIONAL, 'Target platform')
+            ->addArgument('architectures', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Target architectures');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $targets = $this->handleInput($input);
 
-        foreach($targets as $platform => $archs) {
-            foreach($archs as $arch) {
-                $output->writeln("Building for $platform-$arch");
+        foreach ($targets as $platform => $archs) {
+            foreach ($archs as $arch) {
+                $output->writeln("Building for {$platform}-{$arch}");
             }
         }
 
         return Command::SUCCESS;
     }
 
-    protected function handleInput(InputInterface $input) : array {
+    protected function handleInput(InputInterface $input): array
+    {
         // Get platform (from argument or prompt)
         $platform = $input->getArgument('platform') ?: select(
             'Select platform',
@@ -61,22 +54,23 @@ class Build extends Command
         );
 
         // Retrun all available platforms (except the 'all' special case)
-        if($platform === 'all') {
+        if ($platform === 'all') {
             return array_diff_key(self::PLATFORMS, ['all' => null]);
         }
 
         // Get architectures (from arguments or prompt)
         $validArchitectures = self::PLATFORMS[$platform];
         $architectures = $input->getArgument('architectures') ?: multiselect(
-            "Select architectures for $platform",
+            "Select architectures for {$platform}",
             $validArchitectures,
             required: true
         );
 
         // Validate combinations
         foreach ($architectures as $arch) {
-            if (!in_array($arch, $validArchitectures)) {
-                error("Invalid architecture '$arch' for $platform");
+            if (! in_array($arch, $validArchitectures)) {
+                error("Invalid architecture '{$arch}' for {$platform}");
+
                 return Command::FAILURE;
             }
         }
