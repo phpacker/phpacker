2d8b8d6ba027b7ac63ff0031206a9a83a05e8c66
gwleuverink <willem@leuver.ink>
2025-02-14 11:36:26 +0100

    add return expectations

--- src/Contracts/RemoteRepositoryService.php
+++ src/Contracts/RemoteRepositoryService.php
@@ -4,6 +4,7 @@ namespace PHPacker\PHPacker\Contracts;
 
 interface RemoteRepositoryService
 {
+    /** @return array{ tag_name: string, zipball_url: string, assets: array } */
     public function releaseData(): ?array;
 
     public function downloadReleaseAssets(string $destination): string;
