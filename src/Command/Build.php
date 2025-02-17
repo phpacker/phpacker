e3d86249afcf7121c9b871cc803da837844f446e
gwleuverink <willem@leuver.ink>
2025-02-17 15:54:03 +0100

    validate src path

--- src/Command/Build.php
+++ src/Command/Build.php
@@ -3,6 +3,7 @@
 namespace PHPacker\PHPacker\Command;
 
 use PHPacker\PHPacker\Support\Combine;
+use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -59,6 +60,8 @@ class Build extends Command
     {
         $targets = $this->handleInput($input, self::PLATFORMS);
 
+        $this->validateSrcPath($input);
+
         if ($ini = $this->promptIniInput($input)) {
             ConfigManager::set('ini', $ini);
         }
@@ -70,17 +73,31 @@ class Build extends Command
         foreach ($targets as $platform => $archs) {
             foreach ($archs as $arch) {
 
-                Combine::build($arch, $platform, ConfigManager::getRepository());
+                Combine::build($platform, $arch, ConfigManager::getRepository());
             }
         }
     }
 
-    protected function printIniTable(array $ini)
+    private function printIniTable(array $ini)
     {
         $rows = array_map(function ($value, $key) {
             return [$key, $value];
         }, $ini, array_keys($ini));
 
         table(['  directive  ', '    value    '], $rows);
+    }
+
+    private function validateSrcPath(InputInterface $input)
+    {
+        $path = Path::join(getcwd(), $input->getOption('src'));
+
+        if (! file_exists($path)) {
+            throw new CommandErrorException("Source file not found: {$path}");
+        }
+
+        $ext = pathinfo($path, PATHINFO_EXTENSION);
+        if (! in_array($ext, ['php', 'phar'])) {
+            throw new CommandErrorException("Invalid file type: {$path}. Expected a PHP or PHAR file.");
+        }
     }
 }
