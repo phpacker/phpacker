d4f1b8155ae683565017592345cf80e899ea4afe
gwleuverink <willem@leuver.ink>
2025-02-18 14:12:43 +0100

    wip - combine files

--- src/Support/Combine.php
+++ src/Support/Combine.php
@@ -2,15 +2,75 @@
 
 namespace PHPacker\PHPacker\Support;
 
+use Symfony\Component\Filesystem\Path;
+use PHPacker\PHPacker\Command\Download;
 use PHPacker\PHPacker\Support\Config\ConfigRepository;
+use PHPacker\PHPacker\Exceptions\CommandErrorException;
 
 use function Laravel\Prompts\info;
+use function Laravel\Prompts\note;
 
 class Combine
 {
+    const PLATFORM_MAP = [
+        'mac' => 'mac',
+        'linux' => 'linux',
+        'windows' => 'win',
+    ];
+
+    const ARCH_MAP = [
+        'arm' => 'arm64',
+        'x64' => 'x64',
+    ];
+
     public static function build(string $platform, string $arch, ConfigRepository $config)
     {
-        //
         info("Building for {$platform}-{$arch}");
+
+        $phpVersion = $config->get('php');
+        $repository = $config->get('repository');
+
+        $repositoryDir = $repository === Download::DEFAULT_REPOSITORY
+            ? Download::DEFAULT_REPOSITORY_DIR
+            : $repository;
+
+        $repositoryDir = Path::join(APP_DATA, 'binaries', $repositoryDir);
+
+        if (! file_exists($repositoryDir)) {
+            throw new CommandErrorException("Repository {$repositoryDir} does not exit");
+        }
+
+        $binPath = Path::join($repositoryDir, self::PLATFORM_MAP[$platform], self::ARCH_MAP[$arch], (string) $phpVersion, 'micro.sfx');
+        if (! file_exists($binPath)) {
+            throw new CommandErrorException("PHP binary {$binPath} does not exit");
+        }
+
+        // Combine the files
+        $srcPath = Path::join(getcwd(), $config->get('src'));
+        if (! file_exists($binPath)) {
+            throw new CommandErrorException("Source at {$srcPath} does not exit");
+        }
+
+        $outputPath = Path::join(dirname($srcPath), 'build', $platform, "{$platform}-{$arch}"); // TODO: Make build path configurable
+        $iniPart = ''; // TODO Inject INI
+
+        if (PHP_OS_FAMILY === 'Windows') {
+            $outputPath .= '.exe';
+        }
+
+        @mkdir(dirname($outputPath), 0755, recursive: true); //
+        touch($outputPath);
+
+        $combined = file_get_contents($binPath) . $iniPart . file_get_contents($srcPath);
+        $result = file_put_contents($outputPath, $combined);
+
+        if ($result === false) {
+            throw new CommandErrorException('Build failed');
+        }
+
+        // chmod +x
+        chmod($outputPath, 0755);
+
+        note($outputPath);
     }
 }
