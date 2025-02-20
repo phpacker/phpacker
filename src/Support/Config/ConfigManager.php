85c09f6ef27e2b28e39fee35f4a6ac094e8cf421
gwleuverink <willem@leuver.ink>
2025-02-20 16:49:25 +0100

    improve config discovery algorithm

--- src/Support/Config/ConfigManager.php
+++ src/Support/Config/ConfigManager.php
@@ -22,6 +22,8 @@ class ConfigManager
 
     protected static ConfigRepository $repository;
 
+    protected static bool $loaded = false;
+
     const INTERNAL_CONFIG = __DIR__ . '/../../../config/phpacker.json';
 
     const PROXY_METHODS = [
@@ -55,6 +57,11 @@ class ConfigManager
 
         // Dynamically merge config based on command input
         $dispatcher->addListener('console.command', function ($event) {
+            // Guard rediscovery when one command calls another
+            if (self::$loaded) {
+                return;
+            }
+
             $input = $event->getInput();
 
             // Override with discovered or given config file
@@ -71,6 +78,8 @@ class ConfigManager
             self::$repository->merge(
                 array_filter($input->getArguments()),
             );
+
+            self::$loaded = true;
         });
     }
 
@@ -81,68 +90,84 @@ class ConfigManager
     */
 
     /*
-     * When a --config option was given, use it
-     * Otherwise scan the src argument directory
+     * Will look for a config file in the following order
+     *    1. Custom path specified via `--config=path/to/file.json`
+     *    2. `phpacker.json` in the source directory via `--src` option
+     *    3. `phpacker.json` in the current working directory
+     *
+     * This discovery priority logic does complicate things, but I
+     * think the flexibility it grants outweighs the technical debt
      */
     private static function configFromCommand(InputInterface $input): array
     {
-        if (! $input->hasOption('config') && ! $input->hasOption('src')) {
-            return [];
-        }
+        $internalConfig = self::readJsonFile(self::INTERNAL_CONFIG);
 
-        // If a --config option was given
-        $configPath = $input->getOption('config');
+        // 1. Custom path specified via `--config=path/to/file.json`
+        $configPath = $input->hasOption('config')
+            ? $input->getOption('config')
+            : false;
 
         if (is_string($configPath)) {
             info("Using config file at '{$configPath}'");
 
-            return self::convertPaths(
-                self::readJsonFile($configPath),
-                dirname($configPath)
-            );
+            $config = array_merge($internalConfig, self::readJsonFile($configPath));
+
+            return self::convertPaths($config, dirname($configPath));
         }
 
-        // If a --src option was given scan the src dir
-        $sourceFile = $input->getOption('src');
+        // 2. `phpacker.json` in the source directory via `--src` option
+        $sourceFile = $input->hasOption('src')
+            ? $input->getOption('src')
+            : false;
 
         if (is_string($sourceFile)) {
             // Determine the folder of $sourcePath
             $sourceDir = dirname($sourceFile);
-            $projectConfig = Path::join($sourceDir, 'phpacker.json');
+            $configPath = Path::join($sourceDir, 'phpacker.json');
 
             // Use project config paths relative to itself
-            if (file_exists($projectConfig)) {
-                info("Using config file at '{$projectConfig}'");
+            if (file_exists($configPath)) {
+                info("Using config file at '{$configPath}'");
 
-                return self::convertPaths(
-                    self::readJsonFile($projectConfig),
-                    dirname($projectConfig)
-                );
+                $config = array_merge($internalConfig, self::readJsonFile($configPath));
+
+                return self::convertPaths($config, dirname($configPath));
             }
 
-            // Use default config paths relative to --src
-            return self::convertPaths(
-                self::readJsonFile(self::INTERNAL_CONFIG),
-                $sourceDir
-            );
+            // This is key, make sure the defaults are relative to the src,
+            // So if we fall through below, that will be the default.
+            $internalConfig = self::convertPaths($internalConfig, $sourceDir);
         }
 
-        return [];
+        // 3. `phpacker.json` in the current working directory
+        $configPath = Path::join(getcwd(), 'phpacker.json');
+
+        if (file_exists($configPath)) {
+            info("Using config file at '{$configPath}'");
+
+            $config = array_merge($internalConfig, self::readJsonFile($configPath));
+
+            return self::convertPaths($config, getcwd());
+        }
+
+        return $internalConfig;
     }
 
     /*
-     * When a --ini option was given, use it
-     * Otherwise scan the src argument directory
+     * Will look for ini configuration in the following order
+     *    1. Custom path specified via `--ini=path/to/file.ini`
+     *    2. Path specified in discovered config file
+     *    3. `phpacker.ini` in the source directory via `--src` option
+     *    4. `phpacker.ini` in the current working directory
+     *    5. Interactive prompt if `--ini` is passed without a value (handled in build command)
      */
     private static function iniFromCommand(InputInterface $input): array
     {
-        // No --ini configured in command
-        if (! $input->hasOption('ini')) {
-            return [];
-        }
 
-        // If a --ini option was given
-        $iniPath = $input->getOption('ini');
+        // 1. Custom path specified via `--ini=path/to/file.ini`
+        $iniPath = $input->hasOption('ini')
+            ? $input->getOption('ini')
+            : false;
 
         if (is_string($iniPath)) {
             info("Using ini file at '{$iniPath}'");
@@ -150,21 +175,43 @@ class ConfigManager
             return self::readIniFile($iniPath);
         }
 
-        // If a --src option was given scan the src dir
-        $sourceFile = $input->getOption('src');
+        // 2. Path specified in discovered config file
+        $iniPath = self::$repository->get('ini');
+
+        if (is_string($iniPath) && $iniPath != '') {
+            info("Using ini file at '{$iniPath}'");
+
+            return self::readIniFile($iniPath);
+        }
+
+        // 3. `phpacker.ini` in the source directory via `--src` option
+        $sourceFile = $input->hasOption('src')
+            ? $input->getOption('src')
+            : false;
 
         if (is_string($sourceFile)) {
             // Determine the folder of $sourcePath
             $sourceDir = dirname($sourceFile);
-            $projectIni = Path::join($sourceDir, 'phpacker.ini');
+            $iniPath = Path::join($sourceDir, 'phpacker.ini');
 
-            if (file_exists($projectIni)) {
-                info("Using ini file at '{$projectIni}'");
+            // Use project config paths relative to itself
+            if (file_exists($iniPath)) {
+                info("Using ini file at '{$iniPath}'");
 
-                return self::readIniFile($projectIni);
+                return self::readIniFile($iniPath);
             }
         }
 
+        // 4. `phpacker.ini` in the current working directory
+        $iniPath = Path::join($iniPath, 'phpacker.ini');
+
+        if (file_exists($iniPath)) {
+            info("Using ini file at '{$iniPath}'");
+
+            return self::readIniFile($iniPath);
+        }
+
+        // 5. Interactive prompt if `--ini` is passed without a value (handled in build command)
         return [];
     }
 
