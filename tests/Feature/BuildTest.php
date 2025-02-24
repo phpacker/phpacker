f666d6a9962abe1eab90f15a74f3b274655532e0
gwleuverink <willem@leuver.ink>
2025-02-24 22:16:08 +0100

    add failing test

--- tests/Feature/BuildTest.php
+++ tests/Feature/BuildTest.php
@@ -43,3 +43,31 @@ it('can run the executable', function () {
         ->isSuccessful()->toBeTrue()
         ->getOutput()->toContain('Hello World!');
 });
+
+it('injects ini', function () {
+    $buildPath = __DIR__ . '/../_stubs/build';
+
+    command('build', [
+        'platform' => 'all',
+        '--src' => __DIR__ . '/../_stubs/app-with-ini.php',
+        '--dest' => $buildPath,
+        '--ini' => [
+            'TEST1' => 'Lazy Fox',
+            'TEST2' => 'Lorem Ipsum',
+        ],
+        '--quiet',
+    ])->getStatusCode()->toBe(Command::SUCCESS);
+
+    $executable = [
+        'mac-arm' => $buildPath . '/mac/mac-arm',
+        'mac-x64' => $buildPath . '/mac/mac-x64',
+        'linux-arm' => $buildPath . '/linux/linux-arm',
+        'linux-x64' => $buildPath . '/linux/linux-x64',
+        'windows-x64' => $buildPath . '/windows/windows-x64.exe',
+    ][getPlatform() . '-' . getArch()];
+
+    shell($executable)
+        ->isSuccessful()->toBeTrue()
+        ->getOutput()->toContain('TEST1: Lazy Fox')
+        ->getOutput()->toContain('TEST2: Lorem Ipsum');
+});
