b489b77892a2673fc74c9a3208d0d6897319fa06
gwleuverink <willem@leuver.ink>
2025-03-13 01:24:14 +0100

    complete bootstrap feature

---
+++ tests/Feature/DetectExecutionContextTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+
+beforeEach(function () {
+    $this->originalWorkingDirectory = getcwd();
+    chdir(__DIR__);
+});
+
+afterEach(function () {
+    chdir($this->originalWorkingDirectory);
+});
+
+it('detects local environment', function () {
+    shell('php ' . __DIR__ . '/../_stubs/app.php')
+        ->isSuccessful()->toBeTrue()
+        ->getOutput()->toContain('PHPACKER_ENV: local');
+});
+
+it('detects production environment', function () {
+
+    $buildPath = '../_stubs/build';
+
+    command('build', [
+        'platform' => 'all',
+        '--src' => '../_stubs/app.php',
+        '--dest' => $buildPath,
+        '--quiet',
+    ])->getStatusCode()->toBe(Command::SUCCESS);
+
+    $executable = [
+        'mac-arm' => $buildPath . '/mac/mac-arm',
+        'mac-x64' => $buildPath . '/mac/mac-x64',
+        'linux-arm' => $buildPath . '/linux/linux-arm',
+        'linux-x64' => $buildPath . '/linux/linux-x64',
+        'windows-x64' => $buildPath . '/windows/windows-x64.exe',
+    ][getPlatform() . '-' . getArch()];
+
+    shell($executable)
+        ->isSuccessful()->toBeTrue()
+        ->getOutput()->toContain('PHPACKER_ENV: production');
+});
