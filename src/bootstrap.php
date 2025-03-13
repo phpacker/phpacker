30409b865d678a851d9d84e7531e71fd5a0fced4
gwleuverink <willem@leuver.ink>
2025-03-13 01:35:54 +0100

    update comment

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,6 +2,9 @@
 
 /**
  * This file is loaded both via composer & included in the build
+ * so it's always invoked regardless of context
+ *
+ * - as long as the build source includes the composer autoloader
  */
 php_sapi_name() === 'micro'
     ? putenv('PHPACKER_ENV=production')
