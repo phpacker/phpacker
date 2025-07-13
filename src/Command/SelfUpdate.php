2c4fcefa5dffa2d78eec0742f80f6597f928f217
Willem Leuverink <willem@leuver.ink>
2025-07-13 23:34:15 +0200

    V1 (#33)
    * remove composer-installer
    
    * relative install source path
    
    * prep self-update
    
    * cleanup

--- src/Command/SelfUpdate.php
+++ src/Command/SelfUpdate.php
@@ -2,13 +2,15 @@
 
 namespace PHPacker\PHPacker\Command;
 
-use PHPacker\Updater\Updater\UpdateManager;
+use PHPacker\Publisher\Updater\UpdateManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use function Laravel\Prompts\info;
 use function Laravel\Prompts\note;
+use function Laravel\Prompts\spin;
 use function Laravel\Prompts\pause;
 
 #[AsCommand(
@@ -24,11 +26,7 @@ class SelfUpdate extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // $currentVersion = 'v0.3';
-        $currentVersion = $this->getApplication()->getVersion();
-        // @phpstan-ignore class.notFound
-        $updateManager = UpdateManager::make($currentVersion, __DIR__ . '/../../phpacker.json');
-
+        $updateManager = UpdateManager::make(__DIR__ . '/../../phpacker.json');
         $updateData = $updateManager->check();
 
         if (! $updateData->updateAvailable) {
@@ -37,10 +35,19 @@ class SelfUpdate extends Command
             return Command::SUCCESS;
         }
 
-        note("Update available! Do you want to update PHPacker {$updateData->currentVersion} -> {$updateData->latestVersion} ?");
-        pause('Press ENTER to continue.');
+        if ($input->isInteractive()) {
+            note("Update available! Do you want to update PHPacker {$updateData->currentVersion} -> {$updateData->latestVersion} ?");
+            pause('Press ENTER to continue.');
+        }
 
-        $updateManager->update();
+        // TODO: Consider udating the php binaries binaries too
+
+        spin(
+            fn () => $updateManager->update(),
+            "Installing update {$updateData->latestVersion}"
+        );
+
+        info('Update installed successfully!');
 
         return Command::SUCCESS;
     }
